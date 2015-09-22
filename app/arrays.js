62cff5e5cddea516ecdbcfa3246e4e52dd087af7
Eric Oligney <EOengineer@gmail.com>
2015-09-21 23:32:26 -0400

    first few tests on arrays

--- app/arrays.js
+++ app/arrays.js
@@ -3,19 +3,31 @@ exports = (typeof window === 'undefined') ? global : window;
 exports.arraysAnswers = {
 
   removeWithoutCopy : function(arr, item) {
-
+    for (var i = arr.length; i--;) {
+      if (arr[i] === item) {
+        arr.splice(i, 1);
+      }
+    }
+    return arr;
   },
 
   append : function(arr, item) {
-
+    // arr.push(item); <- jquery
+    arr[arr.length] = item;
+    return arr;
   },
 
   truncate : function(arr) {
-
+    // arr.pop(); <- jquery
+    arr.splice(arr.length - 1, 1);
+    return arr;
   },
 
   prepend : function(arr, item) {
-
+    for (var i = arr.length; i--;) {
+      i > 0 ? arr[i + 1] = arr[i] : arr[i] = item;
+    }
+    return arr;
   },
 
   curtail : function(arr) {
